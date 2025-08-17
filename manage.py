d985f20b0110c05ae98b3273f2de111e690754d3
ankit-git <ankitashok.am@gmail.com>
2025-08-18 03:21:40 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
+ 
 def main():
+    # Load environment variables early
+    from dotenv import load_dotenv  # Only if using python-dotenv
+    load_dotenv()
+ 
+    # If you're using python-decouple or django-environ, you might not need this step directly
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoApp.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,7 +19,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
+ 
 if __name__ == '__main__':
     main()
